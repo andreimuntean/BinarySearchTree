a0fd76aa5b2f3be125afa76b687932de73db4ddd
andreimuntean <andrei.muntean.dev@gmail.com>
2015-03-14 23:46:57 +0000

    Created the source file

---
+++ BinarySearchTree/BinarySearchTree.java
@@ -0,0 +1,12 @@
+/**
+ * A node-based binary search tree.
+ *
+ * @author Andrei Muntean
+ */
+public class BinarySearchTree implements Comparable<BinarySearchTree>
+{
+	public int compareTo(BinarySearchTree node)
+	{
+		return 0;
+	}
+}
